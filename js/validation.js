6b65f7b3e76316a0616223124ac25ecbb4ea6523
Matt <m_dittmann@yahoo.com>
2017-07-08 19:19:55 -0700

    update validation.js

--- js/validation.js
+++ js/validation.js
@@ -1,5 +1,9 @@
 (function() {
   document.forms.register.noValidate = true; // disable HTML5 validation using JavaScript instead to make it consistent among browsers
+
+  // A
+
+  // CHECKED AND WORKING
   $('form').on('submit', function(e) {
     var elements = this.elements; // Collection of form controls
     var valid = {}; // Keeps track of each form control being valid or not
@@ -7,14 +11,15 @@
     var isFormValid; // flag to check if whole form is valid
 
     // generic checks
-    for (var i = 0; l = (elements.length - 1); i < l; i++) { // loop through each form control
+    var i;
+    for (i = 0, l = elements.length; i < l; i++) { // loop through each form control
       isValid = validateRequired(elements[i]) && validateTypes(elements[i]); // is the element required / does it have a value? Does the value correspond with the type?
       if (!isValid) {
         showErrorMessage(elements[i]);
       } else {
         removeErrorMessage(elements[i]);
       }
-      valid[elements[i].id] = isVaild; // add element to the valid object
+      valid[elements[i].id] = isValid; // add element to the valid object
     }
 
     // custom validation
@@ -23,6 +28,20 @@
       valid.bio = false;
     } else {
       removeErrorMessage(document.getElementById('bio'));
+    }
+
+    if (!validatePassword()) {
+      showErrorMessage(document.getElementById('password'));
+      valid.password = false;
+    } else {
+      removeErrorMessage(document.getElementById('password'));
+    }
+
+    if (!validateParentsConsent()) {
+      showErrorMessage(document.getElementById('parents-consent'));
+      valid.parentsConsent = false;
+    } else {
+      removeErrorMessage(document.getElementById('parents-consent'));
     }
 
     // did it pass validation and can the form be submitted?
@@ -40,6 +59,9 @@
     }
   });
 
+  // B
+
+  // CHECKED AND WORKING
   // functions for generic checks
   // check if the field is required and if so does it have a value?
   function validateRequired(el) {
@@ -64,19 +86,6 @@
     return !el.value || el.value === el.placeholder;
   }
 
-  function setErrorMessage(el, message) {
-    $(el).data('errorMessage', message);
-  }
-
-  function showErrorMessage(el) {
-    var $el = $(el);
-    var $errorContainer = $el.siblings('.error');
-    if (!errorContainer.length) {
-      $errorContainer = $('<span class="error"></span>').inserAfter($el);
-    }
-    $errorContainer.text($(el).data('errorMessage'));
-  }
-
   function validateTypes(el) {
     if (!el.value) return true; // if element has no value return true and exit function
     var type = $(el).data('type') || el.getAttribute('type'); // get the type of input
@@ -87,6 +96,9 @@
     }
   }
 
+  // C
+
+  // CHECKED AND WORKING
   // functions for custom validation
   // if user is less than 13 years old, check that parents consent checkbox is checked (dependency birthday.js)
   function validateParentsConsent() {
@@ -122,8 +134,34 @@
     return valid;
   }
 
+  // D
 
+  //  CHECKED AND WORKING
+  function setErrorMessage(el, message) {
+    $(el).data('errorMessage', message);
+  }
 
+  function getErrorMessage(el) {
+    return $(el).data('errorMessage') || el.title;
+  }
+
+  function showErrorMessage(el) {
+    var $el = $(el);
+    var errorContainer = $el.siblings('.error.message');
+    if (!errorContainer.length) {
+      errorContainer = $('<span class="error message"></span>').insertAfter($el);
+    }
+    errorContainer.text(getErrorMessage(el));
+  }
+
+  function removeErrorMessage(el) {
+    var errorContainer = $(el).siblings('.error.message');
+    errorContainer.remove();
+  }
+
+  // E
+
+  //CHECKED AND WORKING
   // object for checking types
   var validateType = {
     email: function (el) {
@@ -141,7 +179,7 @@
       return valid;
     },
     date: function (el) {
-      var valid = /^(\d{2}\/\d{2}\/\d{4}) | (\d{4}-\d{2}-\d{2})$/.test(el.value);
+      var valid = /^(\d{2}\/\d{2}\/\d{4})|(\d{4}-\d{2}-\d{2})$/.test(el.value);
       if (!valid) {
         setErrorMessage(el, 'Please enter a valid date');
       }
